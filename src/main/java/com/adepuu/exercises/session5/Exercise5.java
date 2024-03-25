d19b13b49d878bb468ad11187203f1f82822c5d7
001indah <indahbudiati001@gmail.com>
2024-03-25 15:56:41 +0700

    feat(sesion-5 exercise 5): first

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -14,5 +14,46 @@ public class Exercise5 {
      */
     public static void main(String[] args) {
 
+        //case 3.2 Ascending num
+
+        int ascTarget[] = {9, 8, 7, 9, 8, 1, 2};
+        int i, j, temp;
+
+        System.out.println("Case 3.2 the result of ascending short is: ");
+        for (i = 0; i < ascTarget.length; i++) {
+            for (j = ascTarget.length - 1; j > i; j--) {
+                if (ascTarget[j] < ascTarget[j - 1]) {
+                    temp = ascTarget[j];
+                    ascTarget[j] = ascTarget[j - 1];
+                    ascTarget[j - 1] = temp;
+                }
+            }
+        }
+        for (i = 0; i < ascTarget.length; i++) {
+            System.out.print(ascTarget[i] + ",");
+        }
+
+        //case 3.3 Descending num
+
+        int descTarget[] = {9, 8, 7, 9, 8, 1, 2};
+        int k, l, temp2;
+        System.out.println();
+        System.out.println("Case 3.3 the result of descending short is: ");
+
+        for (i = 0; i < descTarget.length; i++) {
+            for (j = descTarget.length - 1; j > i; j--) {
+                if (descTarget[j] > descTarget[j - 1]) {
+                    temp2 = descTarget[j];
+                    descTarget[j] = descTarget[j - 1];
+                    descTarget[j - 1] = temp2;
+                }
+            }
+        }
+        for (i = 0; i < descTarget.length; i++) {
+            System.out.print(descTarget[i] + ",");
+        }
+
+
+
     }
 }
