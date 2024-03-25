bac73cf27cf73298890678eced4a5731eefb9aa0
001indah <indahbudiati001@gmail.com>
2024-03-25 15:19:39 +0700

    feat(sesion-5 exercise 1): first

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,17 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        //case 1.1 looking for the largest num
+        int[] target = {1, 3, 4, 5, 6, 7};
+        int largest = target[0];
+
+        for (int i = 1; i < target.length; i++) {
+            if (target[i] > largest) {
+                largest = target[i];
+            }
+        }
+
+        System.out.println("Case 1.1. the largest num is: "+ largest);
+    }
 }
--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -5,11 +5,11 @@ public class Exercise5 {
      * Java Array Program to sort array in increasing & decreasing order
      *
      * Example 1:
-     * Input: [ 8, 7, 5, 2], direction = “asc”
+     * Input: [ 8, 7, 5, 2], direction = "asc"
      * Output: [ 2, 5, 7, 8 ]
      *
      * Example 2:
-     * Input: [ 8, 7, 5, 2], direction = “desc”
+     * Input: [ 8, 7, 5, 2], direction = "desc"
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
